3c06aace6a7694a3ebb43c4697401eb3a0be4974
Matt Crouch <matt@mattcrouch.net>
2018-08-29 23:08:12 +0100

    Artificial latency

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const argv = require("minimist")(process.argv.slice(2));
+
 const store = require("./store.js");
 
 const express = require("express");
@@ -8,6 +10,14 @@ app.use((req, res, next) => {
 });
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+if (argv.slow) {
+  console.log("Adding artificial latency...");
+
+  app.use((req, res, next) => {
+    setTimeout(next, Math.floor(Math.random() * 3000));
+  });
+}
 
 app.get("/photos", (req, res) => res.json(store.getPhotos()));
 

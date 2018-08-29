10b5e17d0bbd1204e67dfa7c0064b4b78772481a
Matt Crouch <matt@mattcrouch.net>
2018-08-29 22:42:02 +0100

    CORS

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const store = require("./store.js");
 
 const express = require("express");
 const app = express();
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
